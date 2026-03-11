db8fe770e1a2269b59b8c0572fdd4f505c7219be
Anish <anish22075@iiitd.ac.in>
2026-03-11 17:02:43 +0530

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDckK6rAhSrMrk7-nWGfgq_tSQpuwMcnGQ",
-  authDomain: "remote-office-metaverse.firebaseapp.com",
-  projectId: "remote-office-metaverse",
-  storageBucket: "remote-office-metaverse.firebasestorage.app",
-  messagingSenderId: "836212594440",
-  appId: "1:836212594440:web:7ca9c7a0fe870945367ebd"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
